17dba7ba5e779fb7d5a94188e3df1d3c3d663f93
GusttavoCastilho <gustavocvalenciano@gmail.com>
2025-09-19 20:44:13 -0300

    feat: configure absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+          '@theme': './src/theme',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@translations': './src/translations',
+          '@domain': './src/domain',
+          '@api': './src/api',
+          '@store': './src/store',
+          '@services': './src/services',
+        },
+      },
+    ],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,23 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@screens': path.resolve(__dirname, 'src/screens'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@theme': path.resolve(__dirname, 'src/theme'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@translations': path.resolve(__dirname, 'src/translations'),
+      '@domain': path.resolve(__dirname, 'src/domain'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@services': path.resolve(__dirname, 'src/services'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
